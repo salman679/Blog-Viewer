3a815455c3b3a542923b2e1bee90268ea48e9eb2
Salman Izhar <Sbinazhar671@gmail.com>
2025-01-24 16:04:11 +0600

    project done

---
+++ src/app/layout.js
@@ -0,0 +1,34 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { AuthProvider } from "@/Provider/AuthProvider";
+import Header from "@/components/Header/Header";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "blog-viewer",
+  description: "A blog viewer",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <AuthProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <Header />
+          {children}
+        </body>
+      </html>
+    </AuthProvider>
+  );
+}
